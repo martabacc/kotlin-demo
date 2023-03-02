9063080e03c2ce72e31e5c68aebb7a64f6eb5244
Ronauli Silva <ronauli.silva@gojek.com>
2023-03-02 13:43:55 +0700

    feat(init): spinning up db and http query post

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @SpringBootApplication
 class DemoApplication
@@ -13,10 +14,33 @@ fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
 
-@RestController
-class MessageController {
-    @GetMapping("/")
-    fun index(@RequestParam("name") name:String) = "Hello, $name!"
+data class Message(val id: String?, val text: String)
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query(
+        "select * from messages"
+    ) { response, _ -> Message(response.getString("id"), response.getString("text"))}
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values (?, ?)",
+            id,
+            message.text
+        )
+    }
 }
 
-data class Message(val id: String?, val text: String)
+@RestController
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+
